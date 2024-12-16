ea24911fcce18090a92ad35ca59b6db911aa19d5
AyumiDeLima <ayumidelima999@gmail.com>
2024-12-16 17:41:00 -0300

    Atualização

--- AvaliacaoPraticaTrovata/classes/db.php
+++ AvaliacaoPraticaTrovata/classes/db.php
@@ -38,4 +38,6 @@ class db {
         return $this->conn;
     }
 }
+
+
 ?>
---
+++ AvaliacaoPraticaTrovata/src/logout.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tela Home
+ *
+ * Objetivo: Arquivo que processa o logout dessa conta / Destroi e Finaliza o Session 
+ *           Volta para o começo
+ * 
+ */
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    session_unset();
+    session_destroy();
+
+    header('Location: ../index.php');
+    exit;
+}
+
+?>
